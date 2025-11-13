7038970b89591c06feb96c1b9b576e7172e7357f
Diggo <morialvss@proton.me>
2025-11-13 16:32:56 -0300

    criação da segunda tela e funcionalidade de ir para loja

--- src/com/slummanagement/controle/JogoController.java
+++ src/com/slummanagement/controle/JogoController.java
@@ -1,9 +1,9 @@
-//Faz a ponte entre o Simulador.java e a Interface
-
 package com.slummanagement.controle;
 
 import com.slummanagement.modelo.Favela;
 import com.slummanagement.modelo.Simulador;
+import com.slummanagement.visao.DashboardPanel;
+import com.slummanagement.visao.LojaPanel;
 import com.slummanagement.visao.PersonalizacaoPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,35 +11,53 @@ import javax.swing.*;
 
 public class JogoController {
     private JFrame frame;
+    private DashboardPanel dashboardPanel;
     private PersonalizacaoPanel personalizacaoPanel;
     private Simulador simulador;
     
 
-    public JogoController(JFrame frame, PersonalizacaoPanel personalizacaoPanel, Simulador simulador) {
+    public JogoController(JFrame frame, PersonalizacaoPanel personalizacaoPanel, Simulador simulador, DashboardPanel dashboardPanel) {
         this.frame = frame;
+        this.dashboardPanel = dashboardPanel;
         this.personalizacaoPanel = personalizacaoPanel;
         this.simulador = simulador;
     }
 
-    public void inicializar(){
-        personalizacaoPanel.getBotaoJogar().addActionListener(new ActionListener(){
-             @Override
+    public void inicializar() {
+        personalizacaoPanel.getBotaoJogar().addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                String nomeJogador = personalizacaoPanel.getCampoNomeJogador().getText();
-                String nomeFavela = personalizacaoPanel.getCampoNomeFavela().getText();
-                Favela favela = new Favela(nomeJogador, nomeFavela);
+                String nomeJogador = personalizacaoPanel.getCampoNomeJogador().getText().trim();
+                String nomeFavela = personalizacaoPanel.getCampoNomeFavela().getText().trim();
 
                 if (nomeJogador.isEmpty() || nomeFavela.isEmpty()) {
                     JOptionPane.showMessageDialog(frame, "Preencha todos os campos!");
+                    return;
                 }
-                
+
+                Favela favela = new Favela(nomeJogador, nomeFavela);
                 simulador.iniciarJogo(favela);
 
-                PersonalizacaoPanel personalizacaoPanel = new PersonalizacaoPanel(simulador.getMensagemBoasVindas());
-                frame.setContentPane(personalizacaoPanel);
-                frame.revalidate();
-                frame.repaint();
 
+                dashboardPanel.getLabelNomeJogador().setText("Nome: " + nomeJogador);
+                dashboardPanel.getLabelNomeFavela().setText("Nome da Favela: " + nomeFavela);
+
+
+                frame.setContentPane(dashboardPanel);
+                frame.revalidate();
+                frame.repaint(); 
+                
+
+                dashboardPanel.getBotaoLoja().addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        LojaPanel lojaPanel = new LojaPanel(favela, frame, dashboardPanel);
+
+                        frame.setContentPane(lojaPanel);
+                        frame.revalidate();
+                        frame.repaint();
+                    }
+                });
             }
         });
     }
--- src/com/slummanagement/visao/PrincipalFrame.java
+++ src/com/slummanagement/visao/PrincipalFrame.java
@@ -9,16 +9,18 @@ public class PrincipalFrame extends JFrame{
 
     public PrincipalFrame() {
         setTitle("Slum Management - Jogo");
-        setSize(new Dimension(800, 600));
+        setSize(new Dimension(1780, 1000));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
 
+        DashboardPanel dashboardPanel = new DashboardPanel();
+
         PersonalizacaoPanel personalizacaoPanel = new PersonalizacaoPanel();
         Simulador simulador = new Simulador();
 
-        JogoController controller = new JogoController(this, personalizacaoPanel, simulador);
+        JogoController controller = new JogoController(this, personalizacaoPanel, simulador, dashboardPanel);
         controller.inicializar();
 
         setContentPane(personalizacaoPanel);
